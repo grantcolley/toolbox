ba04a8f1ed48387b2d4cc4f2bbe8ca6a6ff82730
grantcolley <grantcolley@yahoo.com>
2023-08-12 13:12:59 +0100

    Create Fibonacci.cs

---
+++ src/Toolbox/Algorithms/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿namespace Toolbox.Algorithms
+{
+    public class Fibonacci
+    {
+        public static IEnumerable<int> Generate(int maxValue = 0)
+        {
+            int previous = 0;
+            int current = 1;
+            int next = 0;
+
+            yield return previous;
+            yield return current;
+
+            while (maxValue == 0
+                || next < maxValue) 
+            {
+                next = previous + current;
+                yield return next;
+
+                previous = current;
+                current = next;
+            }
+        }
+    }
+}
